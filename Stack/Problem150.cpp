fe05cf5c7a736a583345f92578aeac74964fcd9c
Dinh Minh <94245285+dinhminh0307@users.noreply.github.com>
2024-10-15 01:37:55 +0700

    chore: update solution docs

--- Stack/Problem150.cpp
+++ Stack/Problem150.cpp
@@ -10,6 +10,30 @@
 
 using namespace std;
 
+/**
+ * In RPN, you evaluate the expression from left to right:
+
+When you encounter a number, you push it onto a stack.
+When you encounter an operator, you pop the required number of operands from the stack, perform the operation, and push the result back onto the stack.
+Step-by-Step Breakdown:
+"4": It's a number, so push it onto the stack.
+Stack: [4]
+
+"13": It's a number, so push it onto the stack.
+Stack: [4, 13]
+
+"5": It's a number, so push it onto the stack.
+Stack: [4, 13, 5]
+
+"/": This is an operator that requires two operands. Pop the top two numbers from the stack (13 and 5), divide them (13 / 5 = 2), and push the result back onto the stack.
+Stack after division: [4, 2]
+
+"+": This is an operator that also requires two operands. Pop the top two numbers from the stack (4 and 2), add them (4 + 2 = 6), and push the result back onto the stack.
+Stack after addition: [6]
+
+At the end, the only number left in the stack is 6, which is the result of the expression.
+ */
+
 class Solution {
 public:
     bool check_int(string s) {
