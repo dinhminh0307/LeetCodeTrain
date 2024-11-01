2ad95b56a95bdf4e093d8e34f48f06635178f538
dinhminh0307 <doanlamyomostt123@gmail.com>
2024-11-02 01:13:47 +0700

    chore: add comment

--- Linked List/C++/problem143.cpp
+++ Linked List/C++/problem143.cpp
@@ -35,22 +35,22 @@ public:
         // }
         // cout << endl;
         ListNode* tmp = slow->next;
+//         This line disconnects the first half of the list from the second half.
+// By setting slow->next to nullptr, we effectively split the list into two independent parts:
+// The first half, starting from head and ending at slow
+// The second half, starting from tmp
         slow->next = nullptr; // cut the list into 2 halves
-        ListNode* prev = nullptr;
+        ListNode* prev = nullptr; //This pointer will help us build the reversed list.
 
         // when the loop end, the middle node is slow
         // reverse the second half linkedlist
         while(tmp) {
             ListNode* nextNode = tmp->next;
-            tmp->next = prev;
-            prev = tmp;
-            tmp = nextNode;
+            tmp->next = prev; //e reverse the pointer of the current node (tmp) to point back to the previous node (prev), reversing the direction.
+            prev = tmp; //We move prev forward to the current node (tmp). 
+            tmp = nextNode; // We move tmp forward to the next node (nextNode) to continue the loop.
         }
         ListNode* second = prev; // The head of the reversed second half
-        // while(temp) {
-        //     cout << temp->val << endl;
-        //     temp = temp->next;
-        // }
         // now the second half linkedlist is reversed, merged into the first half
         ListNode* first = head;
         while(second) {
