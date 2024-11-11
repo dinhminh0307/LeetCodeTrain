022411e753eaaa56142bfe8433df0a99533ae924
dinhminh0307 <doanlamyomostt123@gmail.com>
2024-11-11 21:15:11 +0700

    feat: add pop dll

--- Linked List/Python/doubly.py
+++ Linked List/Python/doubly.py
@@ -13,8 +13,8 @@ class DoublyLinkedList:
 
     def print_list(self) -> None:
         temp = self.head
-        while temp and temp.next:
-            print(temp.val)
+        while temp:
+            print(temp.value)
             temp = temp.next
 
     def append(self, val)-> bool:
@@ -33,3 +33,66 @@ class DoublyLinkedList:
         
         self.length += 1
         return True 
+    
+    def pop(self) -> bool:
+        if self.tail == None and self.head == None:
+            return False
+        tmp = self.tail
+        # break the connection between 2 nodes
+        if tmp.prev:
+            tmp.prev.next = None
+            self.tail = tmp.prev
+            tmp.prev = None
+        else:
+            self.head = None
+            self.tail = None
+        self.length -= 1
+        return True
+
+# Create a DoublyLinkedList instance
+dll = DoublyLinkedList(1)
+print("Initial list after creating with one element:")
+dll.print_list()
+print("Length:", dll.length)
+print("Head:", dll.head.value if dll.head else None)
+print("Tail:", dll.tail.value if dll.tail else None)
+
+# Append elements to the list
+dll.append(2)
+dll.append(3)
+dll.append(4)
+
+print("\nList after appending 2, 3, and 4:")
+dll.print_list()
+print("Length:", dll.length)
+print("Head:", dll.head.value if dll.head else None)
+print("Tail:", dll.tail.value if dll.tail else None)
+
+# Pop elements from the list
+dll.pop()
+print("\nList after first pop (should remove 4):")
+dll.print_list()
+print("Length:", dll.length)
+print("Head:", dll.head.value if dll.head else None)
+print("Tail:", dll.tail.value if dll.tail else None)
+
+dll.pop()
+print("\nList after second pop (should remove 3):")
+dll.print_list()
+print("Length:", dll.length)
+print("Head:", dll.head.value if dll.head else None)
+print("Tail:", dll.tail.value if dll.tail else None)
+
+dll.pop()
+print("\nList after third pop (should remove 2):")
+dll.print_list()
+print("Length:", dll.length)
+print("Head:", dll.head.value if dll.head else None)
+print("Tail:", dll.tail.value if dll.tail else None)
+
+dll.pop()
+print("\nList after fourth pop (should be empty):")
+dll.print_list()
+print("Length:", dll.length)
+print("Head:", dll.head)
+print("Tail:", dll.tail)
